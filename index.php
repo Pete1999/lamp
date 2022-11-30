d8ec0481edc882d7e8dead2b09e9f2ce9f181123
big pete <peter.shand@sympatico.ca>
2022-11-30 09:51:21 -0500

    Save before re write with PDO

--- index.php
+++ index.php
@@ -8,8 +8,11 @@
   error_reporting(E_ALL);
   $dbconn = pg_connect("host=fedora dbname=shandbox user=peter password=oracle")
            or die('Could not connect: ' . pg_last_error());
-  $query = 'select * from cash_flow';
-  $result = pg_query($dbconn,$query) or die('Query failed: ' . pg_last_error()); 
+  $query = 'begin; select select_candidate_vendor($1,$2,$3);';
+  $result = pg_prepare($dbconn,"ccursor",$query); 
+  $summarize = True;
+  $size = 0;
+  $result = pg_execute($dbconn,"ccursor",array("COSTCO",$size,$summarize));
   echo "<table>\n";
    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     echo "\t<tr>\n";
@@ -19,6 +22,8 @@
     echo "\t</tr>\n";
 }
   echo"</table>\n";
+  pg_free_result($result);
+  pg_close($dbconn);
  ?>
  </body>
 </html>
